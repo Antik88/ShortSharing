dd83129be11676443da21568ec4691b62190886d
Anton <ant1kow22@gmail.com>
2024-07-04 18:04:28 +0300

    add Dtos for evry entity

---
+++ ShortSharing.API/ShortSharing.API/Dtos/CategoryDtos/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using ShortSharing.API.Dtos.TypeDtos;
+
+namespace ShortSharing.API.Dtos.CategoryDtos;
+
+public class CategoryDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public List<TypeDto>? Types { get; set; }
+}
