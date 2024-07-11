d36b04bbd3ff72455627ce0167be8b0aba92bdba
Anton <ant1kow22@gmail.com>
2024-07-11 15:02:08 +0300

    upd validator test

--- ShortSharing.Tests/ValidatorsTests/CreateThingDtoValidatorTest.cs
+++ ShortSharing.Tests/ValidatorsTests/CreateThingDtoValidatorTest.cs
@@ -16,155 +16,54 @@ public class CreateThingDtoValidatorTest
     }
 
     [Fact]
-    public void Validate_ExpectedPriceNotCorrect_ReturnsFalse()
-    {
-        var validator = new CreateThingDtoValidator();
-
-        var model = new CreateThingDto()
-        {
-            Name = "Thing",
-            Description = "valid description",
-            Price = -10.0,
-            CategoryId = Guid.NewGuid(),
-            TypeId = Guid.NewGuid()
-        };
-
-        // Act
-        var result = validator.Validate(model);
-
-        // Assert
-        result.Errors.ShouldNotBeEmpty();
-        result.Errors.ShouldContain(x => x.PropertyName == nameof(model.Price));
-    }
-
-    [Fact]
     public void Validator_ShouldValidateCreateThingDto()
     {
-        var validator = new CreateThingDtoValidator();
+        CreateThingDtoValidator validator = new();
 
-        var model = new CreateThingDto()
-        {
-            Name = "Thing",
-            Description = "valid description",
-            Price = 100.0,
-            CategoryId = Guid.NewGuid(),
-            TypeId = Guid.NewGuid()
-        };
-
-        // Act
-        var result = validator.Validate(model);
-
-        // Assert
-        result.Errors.ShouldBeEmpty();
+        validator.TestValidate(SeedData.GetValidCreateThingDto())
+            .ShouldNotHaveAnyValidationErrors();
     }
 
     [Fact]
     public void Validate_InvalidCreateThingDto_MissingName_ShouldHaveValidationError()
     {
-        var validator = new CreateThingDtoValidator();
+        CreateThingDtoValidator validator = new();
 
-        var model = new CreateThingDto()
-        {
-            Name = string.Empty,
-            Description = "valid description",
-            Price = 100.0,
-            CategoryId = Guid.NewGuid(),
-            TypeId = Guid.NewGuid()
-        };
-
-        // Act
-        var result = validator.Validate(model);
-
-        // Assert
-        result.Errors.ShouldNotBeEmpty();
-        result.Errors.ShouldContain(x => x.PropertyName == nameof(model.Name));
+        validator.TestValidate(SeedData.GetInvalidCreateThingDto_MissingName())
+            .ShouldHaveValidationErrorFor(x => x.Name);
     }
 
     [Fact]
     public void Validate_InvalidCreateThingDto_NegativePrice_ShouldHaveValidationError()
     {
-        var validator = new CreateThingDtoValidator();
+        CreateThingDtoValidator validator = new();
 
-        var model = new CreateThingDto()
-        {
-            Name = "Thing",
-            Description = "valid description",
-            Price = -10.0,
-            CategoryId = Guid.NewGuid(),
-            TypeId = Guid.NewGuid()
-        };
-
-        // Act
-        var result = validator.Validate(model);
-
-        // Assert
-        result.Errors.ShouldNotBeEmpty();
-        result.Errors.ShouldContain(x => x.PropertyName == nameof(model.Price));
+        validator.TestValidate(SeedData.GetInvalidCreateThingDto_NegativePrice())
+            .ShouldHaveValidationErrorFor(x => x.Price);
     }
 
     [Fact]
     public void Validate_InvalidCreateThingDto_EmptyDescription_ShouldHaveValidationError()
     {
-        var validator = new CreateThingDtoValidator();
+        CreateThingDtoValidator validator = new();
 
-        var model = new CreateThingDto()
-        {
-            Name = "Thing",
-            Description = "",
-            Price = 100.0,
-            CategoryId = Guid.NewGuid(),
-            TypeId = Guid.NewGuid()
-        };
-
-        // Act
-        var result = validator.Validate(model);
-
-        // Assert
-        result.Errors.ShouldNotBeEmpty();
-        result.Errors.ShouldContain(x => x.PropertyName == nameof(model.Description));
+        validator.TestValidate(SeedData.GetInvalidCreateThingDto_EmptyDescription())
+           .ShouldHaveValidationErrorFor(x => x.Description);
     }
 
     [Fact]
     public void Validate_InvalidCreateThingDto_InvalidTypeId_ShouldHaveValidationError()
     {
-        var validator = new CreateThingDtoValidator();
-
-        var model = new CreateThingDto()
-        {
-            Name = "Thing",
-            Description = "",
-            Price = 100.0,
-            CategoryId = Guid.NewGuid(),
-            TypeId = Guid.Empty
-        };
-
-        // Act
-        var result = validator.Validate(model);
-
-        // Assert
-        result.Errors.ShouldNotBeEmpty();
-        result.Errors.ShouldContain(x => x.PropertyName == nameof(model.TypeId));
+        CreateThingDtoValidator validator = new();
+        validator.TestValidate(SeedData.GetInvalidCreateThingDto_InvalidTypeId())
+           .ShouldHaveValidationErrorFor(x => x.TypeId);
     }
 
     [Fact]
     public void Validate_InvalidCreateThingDto_InvalidCategoryId_ShouldHaveValidationError()
     {
-        var validator = new CreateThingDtoValidator();
-
-        var model = new CreateThingDto()
-        {
-            Name = "Thing",
-            Description = "",
-            Price = 100.0,
-            CategoryId = Guid.Empty,
-            TypeId = Guid.NewGuid()
-        };
-
-        // Act
-        var result = validator.Validate(model);
-
-        // Assert
-        result.Errors.ShouldNotBeEmpty();
-        result.Errors.ShouldContain(x => x.PropertyName == nameof(model.CategoryId));
+        CreateThingDtoValidator validator = new();
+        validator.TestValidate(SeedData.GetInvalidCreateThingDto_InvalidCategoryId())
+           .ShouldHaveValidationErrorFor(x => x.CategoryId);
     }
 }
