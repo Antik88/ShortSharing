5a1d2b2777b860648a64fc31b4cacfc2bf30a768
Antik88 <50373887+Antik88@users.noreply.github.com>
2024-07-19 13:15:33 +0300

    add integration tests (#25)
    * add integration tests
    
    * fix workflows.yml
    
    * add useInMemory db
    
    * upd build.yml
    
    * fix program class issue
    
    * add test for delete method

--- ShortSharing.API/ShortSharing.API/Program.cs
+++ ShortSharing.API/ShortSharing.API/Program.cs
@@ -4,43 +4,42 @@ using ShortSharing.BLL.Abstractions;
 using ShortSharing.BLL.DI;
 using ShortSharing.BLL.Services;
 
-namespace ShortSharing.API
+namespace ShortSharing.API;
+
+public partial class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        var configurations = builder.Configuration;
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddBusinessLogicDependencies(configurations);
+
+        builder.Services.AddAutoMapper(typeof(ApiProfile));
+
+        builder.Services.AddScoped<IThingsService, ThingsService>();
+
+        var app = builder.Build();
+
+        app.UseMiddleware<LoggingMiddleware>();
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var configurations = builder.Configuration;
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddBusinessLogicDependencies(configurations);
-
-            builder.Services.AddAutoMapper(typeof(ApiProfile));
-
-            builder.Services.AddScoped<IThingsService, ThingsService>();
-
-            var app = builder.Build();
-
-            app.UseMiddleware<LoggingMiddleware>();
-            app.UseMiddleware<ExceptionMiddleware>();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
