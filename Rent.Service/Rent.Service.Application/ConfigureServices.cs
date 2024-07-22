1506eb919d4694c44422eb0c1b283d5f20e89660
Anton <ant1kow22@gmail.com>
2024-07-22 13:30:18 +0300

    set up clean architecture

---
+++ Rent.Service/Rent.Service.Application/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Rent.Service.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection service)
+    {
+        service.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        service.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+        });
+
+        return service;
+    }
+}
