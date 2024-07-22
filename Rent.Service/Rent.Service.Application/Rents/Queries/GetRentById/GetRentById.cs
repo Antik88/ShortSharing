113b4b38ce827e3fecd91349832dcb0543f61a15
Anton <ant1kow22@gmail.com>
2024-07-22 14:16:01 +0300

    add getById command

---
+++ Rent.Service/Rent.Service.Application/Rents/Queries/GetRentById/GetRentById.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Rent.Service.Application.Rents.Queries.GetRents;
+
+namespace Rent.Service.Application.Rents.Queries.GetRentById;
+
+public class GetRentById : IRequest<RentModel>
+{
+   public Guid RentId { get; set; }  
+}
---
+++ Rent.Service/Rent.Service.Application/Rents/Queries/GetRentById/RentIdQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using Rent.Service.Application.Rents.Queries.GetRents;
+using Rent.Service.Domain.Repository;
+
+namespace Rent.Service.Application.Rents.Queries.GetRentById;
+
+public class RentIdQueryHandler(
+    IRentRepository rentRepository,
+    IMapper mapper) : IRequestHandler<GetRentById, RentModel>
+{
+    public async Task<RentModel> Handle(GetRentById request, CancellationToken cancellationToken)
+    {
+        var rent = await rentRepository.GetByIdAsync(request.RentId);
+
+        return mapper.Map<RentModel>(rent);
+    }
+}
