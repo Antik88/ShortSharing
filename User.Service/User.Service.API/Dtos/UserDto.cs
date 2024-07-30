e0be5c24f392f8e3f67389bc83540b2d79ce36e7
Anton <ant1kow22@gmail.com>
2024-07-30 10:17:58 +0300

    fix user dto

--- User.Service/User.Service.API/Dtos/UserDto.cs
+++ User.Service/User.Service.API/Dtos/UserDto.cs
@@ -4,6 +4,6 @@ public class UserDto
 {
     public Guid Id { get; set; }
     public string? AuthId { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 };
