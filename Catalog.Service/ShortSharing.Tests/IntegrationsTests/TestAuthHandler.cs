dd43105e31e5fde34033d591c5469c8279285b45
Anton <ant1kow22@gmail.com>
2024-08-23 15:37:36 +0300

    add auth scheme for integration tests

---
+++ Catalog.Service/ShortSharing.Tests/IntegrationsTests/TestAuthHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace ShortSharing.Tests.IntegrationsTests;
+
+public class TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger, 
+    UrlEncoder encoder) : AuthenticationHandler<AuthenticationSchemeOptions> (options, logger, encoder)
+{
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var identity = new ClaimsIdentity(new[]
+{
+            new Claim(ClaimTypes.NameIdentifier, "testUser"),
+        }, "TestScheme");
+
+        var principal = new ClaimsPrincipal(identity);
+
+        var ticket = new AuthenticationTicket(principal, "TestScheme");
+
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
+    }
+}
