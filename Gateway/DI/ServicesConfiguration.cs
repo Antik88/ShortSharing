72fba400cf2264d911e1020b544220cf1c2cfa61
Antik88 <50373887+Antik88@users.noreply.github.com>
2024-09-03 11:14:44 +0300

    set up gateway (#39)

---
+++ Gateway/DI/ServicesConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+
+namespace Gateway.DI;
+
+public static class ServicesConfiguration
+{
+    public static void AddAuth0Authentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.Authority = configuration["Auth0:Domain"];
+            options.Audience = configuration["Auth0:Audience"];
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                RoleClaimType = "user/roles"
+            };
+        });
+    }
+
+    public static void AddOcelotConfiguration(this IServiceCollection services, IConfigurationManager configuration)
+    {
+        configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+        services.AddOcelot(configuration)
+            .AddCacheManager(x => x.WithDictionaryHandle());
+    }
+}
