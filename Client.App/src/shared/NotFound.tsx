7aad0b3d11462a39cda224f035db5a3d1be8954e
Anton <ant1kow22@gmail.com>
2024-10-03 15:50:08 +0300

    add thing page

--- Client.App/src/shared/NotFound.tsx
+++ Client.App/src/shared/NotFound.tsx
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 import { Box, Container, Typography } from "@mui/material";
+=======
+import { Box, Typography } from "@mui/material";
+>>>>>>> 197610e (add thing page)
 import notFound from '../assets/no-image.jpeg';
 
 export default function NotFound() {
     return (
+<<<<<<< HEAD
         <Container>
             <Box
                 display='flex'
@@ -22,5 +27,24 @@ export default function NotFound() {
                 <img src={notFound} width='400em' />
             </Box>
         </Container>
+=======
+        <Box
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+        >
+            <Typography
+                variant='h5'
+                color='primary'
+                fontFamily='Fahkwang, sans-serif'
+                fontWeight='bold'
+                sx={{ mt: 8 }}
+            >
+                Not Found!
+            </Typography>
+            <img src={notFound} width='400em' />
+        </Box>
+>>>>>>> 197610e (add thing page)
     )
 }
