2ae1a19303cd3285a7654c3faecfb900a3e69d99
Anton <ant1kow22@gmail.com>
2024-11-07 16:19:18 +0300

    add tests

---
+++ Catalog.Service/ShortSharing.Tests/CacheTests/CacheTest.cs
@@ -0,0 +1,47 @@
+﻿using ShortSharing.BLL.Abstractions;
+using NSubstitute;
+using Xunit;
+
+namespace ShortSharing.Tests.CacheTests
+{
+    public class CacheTest
+    {
+        private readonly ICacheService _cacheService;
+
+        public CacheTest()
+        {
+            _cacheService = Substitute.For<ICacheService>();
+        }
+
+        [Theory]
+        [InlineData("testKey", "testData")]
+        public async Task GetData_ShouldReturnCorrectData_WhenDataExists(string key, string expectedData)
+        {
+            _cacheService.GetData<string>(key).Returns(expectedData);
+
+            var result = await _cacheService.GetData<string>(key);
+
+            Assert.Equal(expectedData, result);
+        }
+
+        [Theory]
+        [InlineData("testKey")]
+        public async Task GetData_ShouldReturnNull_WhenNoData(string key)
+        {
+            _cacheService.GetData<string>(key).Returns((string)null);
+
+            var result = await _cacheService.GetData<string>(key);
+
+            Assert.Null(result);
+        }
+
+        [Theory]
+        [InlineData("testKey")]
+        public async Task GetData_ShouldCallGetDataWithCorrectKey(string key)
+        {
+            await _cacheService.GetData<string>(key);
+
+            await _cacheService.Received(1).GetData<string>(key);
+        }
+    }
+}
